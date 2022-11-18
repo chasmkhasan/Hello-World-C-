2fe45b60c6fc3c3aeaf7fc63ff6c44ca20acd4da
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2022-11-18 18:38:13 +0100

    first commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+Console.WriteLine("This is your first Project");
+
+Console.WriteLine("Welcome to real world MK Hasan");
+Console.ReadLine();*/
+
+/*
+int x;
+int y;
+
+x = 100;
+y = x / 2;
+
+Console.WriteLine(y);
+Console.ReadLine(); */
+
+/* Console.Write("Enter FirstNumber: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter SecondNumber: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num1 + num2); */
+
+/*
+int number = 100;
+
+if (number == 10)
+{
+    Console.WriteLine("Number is 10");
+    Console.ReadLine();
+}
+else if (number == 20)
+{
+    Console.WriteLine("Number is 20");
+    Console.ReadLine();
+}
+else if (number == 30)
+{
+    Console.WriteLine("Number is 30");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Invalid Number");
+    Console.ReadLine();
+}*/
+
+// Larger number and lower number
+/* 
+int number = 10;
+
+if (number == 50)
+{
+    Console.WriteLine("The Number is Large!");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Oops. Low number!");
+    Console.ReadLine();
+}*/
+
+
+// print user name in console
+/*
+Console.WriteLine("Hello Everybody, What is your name?");
+
+Console.WriteLine("Please enter you name: ");
+string fullName = Console.ReadLine();
+
+Console.WriteLine("Hello " + fullName);*/
+
+
+// For loop using to show the number
+/*for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.ReadLine();*/
+
+
